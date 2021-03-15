1b46457fab3c3345695321fd3414fa73e1a28265
Jonathan Rein <jrein1296@gmail.com>
2021-03-15 14:45:00 -0400

    Adding first row and columns for user input to index.html.

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+// use jquery switch class effect to change hour color for past, resent and future
+// consider jquery buttons to submit user data to local storage 
+// use bootstrap to build time blocks

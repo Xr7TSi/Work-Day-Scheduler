7f7e3be06e22faed76bf81261afb7972d76c6a76
Jonathan Rein <jrein1296@gmail.com>
2021-03-15 20:49:38 -0400

    Adding checkHour functions which evaluates hour and sets background color.

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,18 @@
 var showCurrentDay = document.getElementById("showCurrentDay")
 var dayIndex = moment().day()
 var currentHour = moment().format("ha")
+var currentHourIndex = moment().hour()
+var nineAm = document.getElementById("nineAm")
+var tenAm = document.getElementById("tenAm") 
+var elevenAm = document.getElementById("elevenAm")  
+var twelvePm = document.getElementById("twelvePm")
+var onePm = document.getElementById("onePm")  
+var twoPm = document.getElementById("twoPm") 
+var threePm = document.getElementById("threePm") 
+var fourPm = document.getElementById("fourPm") 
+var fivePm = document.getElementById("fivePm") 
 
-
-
+// displays current day of week
 function showDayOfWeek()
  {
     if (dayIndex = 0) {
@@ -33,4 +42,90 @@ function showDayOfWeek()
     showCurrentDay.textContent = todayIs;
 } showDayOfWeek()
 
+
+
+// "#e97451" Orange
+//  "#AAAA" Gray
+// "#84d23c" Green
+
+function checkHour9() {
+    if (currentHourIndex === 9) {
+        nineAm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >9) {
+        nineAm.style.backgroundColor = "#AAAA"
+    } else (nineAm.style.backgroundColor = "#84d23c" )
+} checkHour9() 
+
+function checkHour10() {
+    if (currentHourIndex === 10) {
+        tenAm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >10) {
+        tenAm.style.backgroundColor = "#AAAA"
+    } else (tenAm.style.backgroundColor = "#84d23c")
+} checkHour10() 
+
+function checkHour11() {
+    if (currentHourIndex === 11) {
+        elevenAm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >11) {
+        elevenAm.style.backgroundColor = "#AAAA"
+    } else (elevenAm.style.backgroundColor = "#84d23c")
+} checkHour11() 
+
+function checkHour12() {
+    if (currentHourIndex === 12) {
+        twelvePm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >12) {
+        twelvePm.style.backgroundColor = "#AAAA"
+    } else (twelvePm.style.backgroundColor = "#84d23c")
+} checkHour12() 
+
+function checkHour13() {
+    if (currentHourIndex === 13) {
+        onePm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >13) {
+        onePm.style.backgroundColor = "#AAAA"
+    } else ( onePm.style.backgroundColor = "#84d23c")
+} checkHour13() 
+
+function checkHour14() {
+    if (currentHourIndex === 14) {
+        twoPm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >14) {
+        twoPm.style.backgroundColor = "#AAAA"
+    } else (twoPm.style.backgroundColor = "#84d23c")
+} checkHour14() 
+
+function checkHour15() {
+    if (currentHourIndex === 15) {
+        threePm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >15) {
+        threePm.style.backgroundColor = "#AAAA"
+    } else (threePm.style.backgroundColor = "#84d23c")
+} checkHour15() 
+
+function checkHour16() {
+    if (currentHourIndex === 16) {
+        fourPm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >16) {
+        fourPm.style.backgroundColor = "#AAAA"
+    } else (fourPm.style.backgroundColor = "#84d23c")
+} checkHour16() 
+
+function checkHour17() {
+    if (currentHourIndex === 17) {
+        fivePm.style.backgroundColor = "#e97451"
+    }  else 
+    if (currentHourIndex >17) {
+        fivePm.style.backgroundColor = "#AAAA"
+    } else (fivePm.style.backgroundColor = "#84d23c")
+} checkHour17() 
 
---
+++ assets/scriptSandbox.js
@@ -0,0 +1,36 @@
+// use jquery switch class effect to change hour color for past, resent and future
+// consider jquery buttons to submit user data to local storage 
+// use bootstrap to build time blocks
+var showCurrentDay = document.getElementById("showCurrentDay")
+var dayIndex = moment().day()
+var currentHour = moment().format("ha")
+
+
+
+function showDayOfWeek()
+ {
+    if (dayIndex = 0) {
+        todayIs = "Sunday"
+    } else
+    if (dayIndex = 1) {
+        todayIs = "Monday"
+    } else
+    if (dayIndex = 2) {
+        todayIs = "Tuesday"
+    } else
+    if (dayIndex = 3) {
+        todayIs = "Wednesday"
+    } else
+    if (dayIndex = 3) {
+        todayIs = "Thursday"
+    } else
+    if (dayIndex = 5) {
+        todayIs = "Friday"
+    } else
+    if (dayIndex = 6) {
+        todayIs = "Saturday"
+    } 
+    showCurrentDay.textContent = todayIs;
+} showDayOfWeek()
+
+
